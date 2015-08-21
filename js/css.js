a4247bd56c5de5bd31af50ec44aafb21d6015156
Dario Camacho <dcamacho2@outlook.com>
2015-08-21 16:04:57 -0400

    minor refinements

--- js/css.js
+++ js/css.js
@@ -1,5 +1,5 @@
 window.onload = function() {
-	var background = chrome.extension.getURL('img/background.png');
+	var background = chrome.extension.getURL('img/noon.png');
 	document.body.style.background = "url(" + background + ")";
 	console.log(document.body.style);
 }
